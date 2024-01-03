8fe56f4be2dcbc31f911a7a87f204928e97edf89
Robson Fernandes <robson217@gmail.com>
2024-01-02 21:19:11 -0300

    Modificando 9

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ if (process.env.NODE_ENV === "production") {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Bot listening on port ${PORT}`);
-      const analise = setInterval(setIntervalo, 60000)//480000
+      const analise = setInterval(setIntervalo, 10000)//480000
     });
 } else {
     // Use Long Polling for development
