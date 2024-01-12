c1f5536d448f82936fcec16983aaa48d5194c1f6
Robson Fernandes <robson217@gmail.com>
2024-01-12 13:21:06 -0300

    Fev-2024_02

--- modulos/variaveis.js
+++ modulos/variaveis.js
@@ -56,105 +56,25 @@ let arrayDesmontagens = [b3saD, ggbrD, abevD, mgluD, valeD]
 //************************************************************************************************** */
 // Estruturas que podem ser montadas
 // BBDC4
-let bbdcM1 = ["BBDCA174", "Compra", "BBDC4"] 
-let bbdcM2 = ["BBDCM157", "Compra", "BBDC4"]
-let bbdcM = {
+let bbdcM1 = ["BBDCC170", "Compra", "BBDC4"] 
+let bbdcM2 = ["BBDCB166", "Venda", "BBDC4"]
+let bbdcMM1= {
     status: false,
     opcoes: [bbdcM1, bbdcM2],
-    gasto: 0.26,
+    gasto: 0.27,
     descricao: 'Compra de Call e Put'
 }
-// ITSA4
-let itsaM1 = ["ITSAA130", "Compra", "ITSA4"] 
-let itsaM2 = ["ITSAM120", "Compra", "ITSA4"]
-let itsaM = {
+
+let bbdcM3 = ["BBDCC164", "Compra", "BBDC4"] 
+let bbdcM4 = ["BBDCB16", "Venda", "BBDC4"]
+let bbdcMM2 = {
     status: false,
-    opcoes: [itsaM1, itsaM2],
-    gasto: 0.10,
-    ativo: 'ITSA4',
-    descricao: 'Compra de Call / Put'
-}
-// B3SA3
-let b3saM1 = ["B3SAA155", "Compra", "B3SA3"] 
-let b3saM2 = ["B3SAM140", "Compra", "B3SA3"]
-let b3saM = {
-    status: false,
-    opcoes: [b3saM1, b3saM2],
-    gasto: 0.26,
-    ativo: 'B3SA3',
+    opcoes: [bbdcM3, bbdcM4],
+    gasto: 0.27,
     descricao: 'Compra de Call e Put'
 }
-//GGBR4
-let ggbrM1 = ["GGBRA248", 1, 1, "Compra", "GGBR4"]
-let ggbrM2 = ["GGBRM225", 1, 1, "Compra", "GGBR4"]
-let ggbrM = {
-    status: false,
-    opcoes: [ggbrM1, ggbrM2],
-    gasto: -0.41,
-    ativo: 'GGBR4',
-    descricao: 'Compra de Call e Put'
-}
-//ABEV3
-let abevM1 = ["ABEVA148", 1, 1, "Compra", "ABEV3"]
-let abevM2 = ["ABEVM138", 1, 1, "Compra", "ABEV3"]
-let abevM = {
-    status: false,
-    opcoes: [abevM1, abevM2],
-    gasto: -0.18,
-    ativo: 'ABEV3',
-    descricao: 'Compra de Call e Put'
-}
-//MGLU3
-let mgluM1 = ["MGLUA245", 1, 1, "Compra", "MGLU3"]
-let mgluM2 = ["MGLUM20", 1, 1, "Compra", "MGLU3"]
-let mgluM = {
-    status: false,
-    opcoes: [mgluM1, mgluM2],
-    gasto: -0.25,
-    ativo: 'MGLU3',
-    descricao: 'Compra de Call e Put'
-}
-//SUZB3
-let suzbM1 = ["SUZBA565", 1, 1, "Compra", "SUZB3"]
-let suzbM2 = ["SUZBM510", 1, 1, "Compra", "SUZB3"]
-let suzbM = {
-    status: true,
-    opcoes: [suzbM1, suzbM2],
-    gasto: -0.67,
-    ativo: 'SUZB3',
-    descricao: 'Compra de Call e Put'
-}
-//BOVA11
-let bovaM1 = ["BOVAA132", 1, 1, "Compra", "BOVA11"]
-let bovaM2 = ["BOVAM116", 1, 1, "Compra", "BOVA11"]
-let bovaM = {
-    status: true,
-    opcoes: [bovaM1, bovaM2],
-    gasto: -0.91,
-    ativo: 'BOVA11',
-    descricao: 'Compra de Call e Put'
-}
-//PETR4
-let petrM1 = ["PETRA383", 1, 1, "Compra", "PETR4"]
-let petrM2 = ["PETRM371", 1, 1, "Compra", "PETR4"]
-let petrM = {
-    status: true,
-    opcoes: [petrM1, petrM2],
-    gasto: -0.91,
-    ativo: 'PETR4',
-    descricao: 'Compra de Call e Put'
-}
-//VALE3
-let valeM1 = ["VALEA826", 1, 1, "Compra", "VALE3"]
-let valeM2 = ["VALEM756", 1, 1, "Compra", "VALE3"]
-let valeM = {
-    status: true,
-    opcoes: [valeM1, valeM2],
-    gasto: -0.98,
-    ativo: 'VALE3',
-    descricao: 'Compra de Call e Put'
-}
-let arrayMontagens = [bbdcM, itsaM, b3saM, ggbrM, abevM, mgluM, suzbM, bovaM, petrM, valeM]
+
+let arrayMontagens = [bbdcMM1, bbdcMM2]
 //************************************************************************************************** */
 //Criando uma biblioteca de opções
 let tickets = ['BBDC4', 'ITSA4', 'B3SA3', 'GGBR4', 'ABEV3', 'MGLU3', 'SUZB3', 'BOVA11', 'PETR4', 'VALE3']
